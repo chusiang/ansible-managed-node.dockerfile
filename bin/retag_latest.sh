f2cfb5d52d47c8b563d35edbe5a7b44b601c0f85
Chu-Siang Lai <chusiang.lai@gmail.com>
2018-01-11 10:42:06 +0800

    Reuse the docker image name variable

--- bin/retag_latest.sh
+++ bin/retag_latest.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-echo 'Pull alpine image.'
-docker pull chusiang/ansible-managed-node:alpine-3.4
+DOCKER_IMAGE="chusiang/ansible-managed-node"
+DOCKER_TAG="alpine-3.6"
+
+echo '===> Pull current image ...'
+docker pull $DOCKER_IMAGE:$DOCKER_TAG
 echo
 
-echo 'Tag alpine image to latest.'
-docker tag  chusiang/ansible-managed-node:alpine-3.4 chusiang/ansible-managed-node:latest
+echo '===> Tag current image to latest ...'
+docker tag $DOCKER_IMAGE:$DOCKER_TAG $DOCKER_IMAGE:latest
 echo
 
-echo 'Push the latest tag.'
-docker push chusiang/ansible-managed-node:latest
+echo '===> Push the latest tag ...'
+docker push $DOCKER_IMAGE:latest
 echo
 
-echo 'Remove old image.'
-docker rmi $(docker images | grep 'chusiang/ansible-managed-node' | grep '<none>' | awk '{ print $3 }')
+echo '===> Remove old image ...'
+docker rmi $(docker images | grep $DOCKER_IMAGE | grep '<none>' | awk '{ print $3 }')
